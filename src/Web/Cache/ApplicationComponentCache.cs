7f82bdb73d875e0199c7c51027713b0e5d302836
hamed <hamed@HAMED-PC>
2017-01-21 11:05:42 +0330

    Migrated Web project contents from Jahanjooy lobrary

---
+++ src/Web/Cache/ApplicationComponentCache.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using Appson.Composer.Cache;
+
+namespace Appson.Composer.Web.Cache
+{
+	[Contract]
+	[Component]
+	[ComponentCache(typeof(StaticComponentCache))]
+	public class ApplicationComponentCache : IComponentCache
+	{
+		private const string AppKeyPrefix = "Appson.Composer.Web.Cache.Application.";
+
+		#region Implementation of IComponentCache
+
+		public ComponentCacheEntry GetFromCache(ContractIdentity contract)
+		{
+			return HttpContext.Current.Application.Get(GetIdentityString(contract)) as ComponentCacheEntry;
+		}
+
+		public void PutInCache(ContractIdentity contract, ComponentCacheEntry entry)
+		{
+			HttpContext.Current.Application.Set(GetIdentityString(contract), entry);
+		}
+
+		public object SynchronizationObject => HttpContext.Current.Application;
+
+	    #endregion
+
+		#region Private helper methods
+
+		private static string GetIdentityString(ContractIdentity contract)
+		{
+			if (contract == null)
+				return AppKeyPrefix + "<NullContract>";
+
+			if (contract.Type == null)
+				return AppKeyPrefix + "<NullType>";
+
+			if (contract.Name == null)
+				return AppKeyPrefix + contract.Type.FullName;
+
+			return AppKeyPrefix + contract.Type.FullName + "," + contract.Name;
+		}
+
+		#endregion
+	}
+}
---
+++ src/Web/Configuration/CompositionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace Appson.Composer.Web.Configuration
+{
+	public class CompositionConfiguration : ConfigurationSection
+	{
+		[ConfigurationProperty("setResolver", DefaultValue = "true", IsRequired = false)]
+		public Boolean SetResolver
+		{
+			get { return (Boolean) this["setResolver"]; }
+			set { this["setResolver"] = value; }
+		}
+
+		[ConfigurationProperty("registerDefaultComponents", DefaultValue = "true", IsRequired = false)]
+		public Boolean RegisterDefaultComponents
+		{
+			get { return (Boolean) this["registerDefaultComponents"]; }
+			set { this["registerDefaultComponents"] = value; }
+		}
+
+		[ConfigurationProperty("setupCompositionXmls", IsDefaultCollection = false)]
+		public SetupCompositionXmlsCollection SetupCompositionXmls => (SetupCompositionXmlsCollection)base["setupCompositionXmls"];
+	}
+}
