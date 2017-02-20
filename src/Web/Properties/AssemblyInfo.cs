69c6b5334190eed6bf6ff3b4dcb9b6e467afc770
hamed <hamed@HAMED-PC>
2017-02-20 11:20:08 +0330

    Bump version to 1.1.1

--- src/Web/Properties/AssemblyInfo.cs
+++ src/Web/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ae64632b-8e40-4cb7-9356-9ffd40352688")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
