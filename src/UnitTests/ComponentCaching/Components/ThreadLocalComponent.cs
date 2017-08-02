51a6afe6b5cdd3fa941fc00d0f182aa0ea87629f
hamed <hamed@HAMED-PC>
2017-08-02 20:13:49 +0430

    Add a simple unit test for ThreadLocalComponentCache

---
+++ src/UnitTests/ComponentCaching/Components/ThreadLocalComponent.cs
@@ -0,0 +1,12 @@
+﻿using Appson.Composer.Cache;
+
+namespace Appson.Composer.UnitTests.ComponentInstantiations.Components
+{
+    [Contract]
+    [Component]
+    [ComponentCache(typeof(ThreadLocalComponentCache))]
+    public class ThreadLocalComponent : ISomeContract, IAnotherContract
+    {
+        
+    }
+}
